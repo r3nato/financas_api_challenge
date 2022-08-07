a3e11a4c0bf2ac0f777ba426cee11bb27e9f9d89
Renato Rodrigues Oliveira da Silva <renato.oliveira@ifsp.edu.br>
2022-08-06 23:49:20 -0300

    Incluir validação nos cadastros

--- financas_api/financas/serializers.py
+++ financas_api/financas/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueForMonthValidator
 from  .models import Despesa, Receita
 
 
@@ -6,9 +7,26 @@ class DespesaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Despesa
         fields = ['id', 'data', 'valor', 'descricao']
+        validators = [
+            UniqueForMonthValidator(
+                queryset=Despesa.objects.all(),
+                field='descricao',
+                date_field='data',
+                message='Um mesmo mês não pode ter duas despesas com mesma descrição.'
+            )
+        ]
+        
 
 
 class ReceitaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Receita
         fields = '__all__'
+        validators = [
+            UniqueForMonthValidator(
+                queryset=Receita.objects.all(),
+                field='descricao',
+                date_field='data',
+                message='Um mesmo mês não pode ter duas receitas com mesma descrição.'
+            )
+        ]
