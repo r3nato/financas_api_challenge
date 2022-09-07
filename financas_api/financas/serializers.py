bee126a54396a3ed7f21eb42d9df51f2995634fd
Renato Rodrigues Oliveira da Silva <renato.oliveira@ifsp.edu.br>
2022-09-07 18:48:30 -0300

    Modificar visualização da categoria de despesa

--- financas_api/financas/serializers.py
+++ financas_api/financas/serializers.py
@@ -15,7 +15,11 @@ class DespesaSerializer(serializers.ModelSerializer):
                 message='Um mesmo mês não pode ter duas despesas com mesma descrição.'
             )
         ]
-        
+    
+    def to_representation(self, instance):
+        repr = super().to_representation(instance)
+        repr['categoria'] = instance.get_categoria_display()
+        return repr
 
 
 class ReceitaSerializer(serializers.ModelSerializer):
